836cdbc944f9da71a0e82a9a4d166877e333ed45
dbehdrnr0202 <bb06001@naver.com>
2023-04-03 22:47:06 +0900

    "2023.04.03" pp64. need [joining a session]

--- src/main/java/CMClientApp.java
+++ src/main/java/CMClientApp.java
@@ -1,8 +1,10 @@
 import kr.ac.konkuk.ccslab.cm.*;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 
 import javax.imageio.IIOException;
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
@@ -10,6 +12,15 @@ import java.util.Scanner;
 public class CMClientApp {
     private CMClientStub m_clientStub;
     private CMClientEventHandler m_eventHandler;
+
+
+
+    //command num
+    private final int STARTCM = 1;
+    private final int DEFAULT_LOGIN = 2;
+    private final int SYNC_LOGIN = 21;
+    private final int PRINTALLMENU = 0;
+
 
     public CMClientApp(){
         m_clientStub = new CMClientStub();
@@ -24,18 +35,12 @@ public class CMClientApp {
     public static void main(String[] args)  {
         CMClientApp client = new CMClientApp();
         CMClientStub cmStub = client.getClientStub();
-        CMClientEventHandler eventHandler = client.getClientEventHandler();
-        cmStub.setAppEventHandler(eventHandler);//여기가 왜?
+        cmStub.setAppEventHandler(client.getClientEventHandler());//여기가 왜?
         /*
         * Provided: CMClientEventHandler
         * required:  CMAppEventHandler
         */
-        String userName = null;
-        String userPassword = null;
-        Scanner scanner = new Scanner(System.in);
-
-        boolean ret = false;
-        ret = cmStub.startCM();
+        boolean ret = cmStub.startCM();
 
         if (ret)
             System.out.println("init success");
@@ -44,6 +49,62 @@ public class CMClientApp {
             return;
         }
         //
+
+        System.out.println("System terminated");
+    }
+    public void startMainSession() {
+        System.out.println("client application starts.");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner m_scan = new Scanner(System.in);
+        String strInput = null;
+        int nCommand = -1;
+        while(m_bRun) {
+            System.out.println("Type \"0\" for menu.");
+            System.out.print("> ");
+            try {
+                strInput = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            try {
+                nCommand = Integer.parseInt(strInput);
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect command number!");
+                continue;
+            }
+
+            switch (nCommand) {
+                case PRINTALLMENU:
+                    printAllMenus();
+                    break;
+                case STARTCM:
+                    startCM();
+                    break;
+                case DEFAULT_LOGIN:
+                    defaultLogin();
+                    break;
+                case SYNC_LOGIN:
+                    syncLogin();
+                    break;
+                default:
+            }
+        }
+    }
+
+    public void printAllMenus() {
+
+    }
+    public void startCM()   {
+
+    }
+    public void defaultLogin() {
+        String userName = null;
+        String userPassword = null;
+        Console console = System.console();
+
+        boolean ret = false;
         System.out.print("Enter user name: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try{
@@ -61,16 +122,52 @@ public class CMClientApp {
         //
         System.out.println("user name: "+userName);
         System.out.println("password: "+userPassword);
+
+        //default login
         ret = m_clientStub.loginCM(userName, userPassword);
         if (ret)
             System.out.println("successfully sent the login req");
-        else {
+        else
             System.err.println("failed to sent the login req");
-            return;
-        }
-
-        System.out.println("Press enter to execute next API");
-        scanner.nextLine();
-
+        return;
     }
+    public void syncLogin() {
+        String userName = null;
+        String userPassword = null;
+        Console console = System.console();
+
+        boolean ret = false;
+        System.out.print("Enter user name: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try{
+            userName = br.readLine();
+            if (console==null)  {
+                System.out.print("Enter password: ");
+                userPassword = br.readLine();
+            }
+            else
+                userPassword = new String(console.readPassword("password: "));
+        }
+        catch (IOException e)  {
+            e.printStackTrace();
+        }
+        //
+        System.out.println("user name: "+userName);
+        System.out.println("password: "+userPassword);
+
+        CMSessionEvent loginAckEvent = null;
+        loginAckEvent = m_clientStub.syncLoginCM(userName, userPassword);
+        if(loginAckEvent != null) {
+            if(loginAckEvent.isValidUser() == 0)
+                System.err.println("This client fails authentication by the default server!");
+            else if(loginAckEvent.isValidUser() == -1)
+                System.err.println("This client is already in the login-user list!");
+            else
+                System.out.println("This client successfully logs in to the default server.");
+        }
+        else
+            System.err.println("failed the login request!");
+    }
+    //after login process has completed, a client app must join a session and a group of CM to finish entering the CM network
+
 }
--- src/main/java/CMClientEventHandler.java
+++ src/main/java/CMClientEventHandler.java
@@ -1,8 +1,12 @@
+import kr.ac.konkuk.ccslab.cm.entity.CMSessionInfo;
+import kr.ac.konkuk.ccslab.cm.event.handler.CMEventHandler;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
 import kr.ac.konkuk.ccslab.cm.info.*;
 import kr.ac.konkuk.ccslab.cm.event.*;
 
-public class CMClientEventHandler {
+import java.util.Iterator;
+
+public class CMClientEventHandler implements CMEventHandler {
     private CMClientStub m_clientStub;
     public CMClientEventHandler(CMClientStub stub) {
         m_clientStub = stub;
@@ -21,18 +25,42 @@ public class CMClientEventHandler {
         CMSessionEvent se = (CMSessionEvent)cme;
         switch(se.getID()) {
             case CMSessionEvent.LOGIN_ACK:
-                if(se.isValidUser() == 0)   {
-                    System.err.println("This client fails authentication by the default server!");
-                }
-                else if(se.isValidUser() == -1) {
-                    System.err.println("This client is already in the login-user list!");
-                }
-                else {
-                    System.out.println("This client successfully logs in to the default server.");
-                }
+                processLOGIN_ACK(se);
+                break;
+            case CMSessionEvent.RESPONSE_SESSION_INFO:
+                processRESPONSE_SESSION_INFO(se);
                 break;
             default:
                 return;
+        }
+    }
+    private  void processLOGIN_ACK(CMSessionEvent se)   {
+        //0: user authentication failed
+        if(se.isValidUser() == 0)   {
+            System.err.println("This client fails authentication by the default server!");
+        }
+        //-1: same user already logged in
+        else if(se.isValidUser() == -1) {
+            System.err.println("This client is already in the login-user list!");
+        }
+        //1: valied user
+        else if (se.isValidUser()==1){
+            System.out.println("This client successfully logs in to the default server.");
+        }
+        else {
+            System.err.println("Wrong isValidUser() rtn.");
+        }
+
+    }
+    //Receiving session info
+    private void processRESPONSE_SESSION_INFO(CMSessionEvent se)    {
+        Iterator<CMSessionInfo> iter = se.getSessionInfoList().iterator();
+        System.out.format("%-60s%n", "------------------------------------------------------------");
+        System.out.format("%-20s%-20s%-10s%-10s%n", "name", "address", "port", "user num");
+        System.out.format("%-60s%n", "------------------------------------------------------------");
+        while(iter.hasNext())   {
+            CMSessionInfo tInfo = iter.next();
+            System.out.format("%-20s%-20s%-10d%-10d%n", tInfo.getSessionName(), tInfo.getAddress(), tInfo.getPort(), tInfo.getUserNum());
         }
     }
 }
